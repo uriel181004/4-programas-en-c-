36abd96ec4fa921547a532610a857f4244d982fb
uriel181004 <82549982+uriel181004@users.noreply.github.com>
2021-05-03 00:43:18 -0500

    Add files via upload

---
+++ programa en c8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main ()
+{
+    /*Amador Salinas Manuel Uriel
+    Equpo AMUKE
+    2 may 2021
+    Programa 7. Pide 2 valores reales, calcula, multiplica divide y muestra resultado
+    */
+
+
+    float kg, precio;
+    printf ("\n\n Programa 7");
+    printf ("\n\n comprar tortillas");
+    printf ("\n\n Precio de las tortillas 1kg por $15");
+    printf ("\n\n ingresa los kilos de tortilla quieres");
+    scanf ("%f",&kg);
+    precio=kg*15;
+    printf ("\n\n El resultado es %f", precio);
+
+
+    return 0;
+}
