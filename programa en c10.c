2367e3383a71417317143699a27034fb0359948b
uriel181004 <82549982+uriel181004@users.noreply.github.com>
2021-05-03 17:47:15 -0500

    Add files via upload

---
+++ programa en c10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main ()
+{
+    /*Manuel Uriel Amador Salinas
+    Equipo AMUKE
+    30 abr 21
+    Programa metros2. Pide dos valores reales, multiplica y muestra resultado.
+    */
+
+    float metrosdelargo, metrosdeancho;
+    printf ("\n\n Programa metros cuadrados");
+    printf ("\n\n Ingrese los metrosdelargo que trenga su terreno");
+    scanf ("%f",&metrosdelargo);
+    printf ("\n\n Ingrese los metrosdeancho que tenga su terreno");
+    scanf ("%f",&metrosdeancho);
+    metroscuadrados=metrosdelargo*metrosdeancho
+    printf ("\n\n El resultado final es &f", metroscuadrados);
+
+    return 0;
+}
